01f116fc3a443808fb6909061a49296cbeee7227
teklynk <teklynk@protonmail.com>
2024-08-16 14:53:00 -0400

    cleanup javascript

--- vite-plugin-generate-thumbnails.js
+++ vite-plugin-generate-thumbnails.js
@@ -5,17 +5,15 @@ import ffmpeg from 'fluent-ffmpeg';
 function generateThumbnails() {
     const videoDir = resolve('public/videos');
     const thumbnailDir = resolve('public/thumbnails');
-    fs.ensureDirSync(thumbnailDir);
-
     const videos = fs.readdirSync(videoDir).filter(file => file.endsWith('.mp4'));
+    fs.ensureDirSync(thumbnailDir);
 
     videos.forEach(video => {
         const videoPath = resolve(videoDir, video);
-        const thumbnailPath = resolve(thumbnailDir, `${video.split('.mp4')[0]}_thumbnail.png`);
 
         ffmpeg(videoPath)
             .screenshots({
-                timestamps: ['10'],
+                timestamps: ['30'],
                 filename: `${video.split('.mp4')[0]}_thumbnail.png`,
                 folder: thumbnailDir,
                 size: '1280x720'
